1f88a351c8b98a06fb57fe5085a72aa6b64cc58d
Jasurbek <jasurdev1604@gmail.com>
2024-08-15 01:44:48 +0500

    new

--- src/components/FAQ/index.jsx
+++ src/components/FAQ/index.jsx
@@ -1,8 +1,52 @@
-import React from "react";
-import { Container } from "./style";
+import React, { useState } from "react";
+import { Container, Accardion, Arrow } from "./style";
 
 const FAQ = () => {
-  return <Container></Container>;
+  const [open, setOpen] = useState(null);
+  const data = [
+    {
+      title: "Do you have specific princing plans to show?",
+      desc: "Donec rutrum, mauris at blandit convallis, orci nulla volutpat sapien, id pulvinar leo ligula eget nunc. In quis magna magna. Nullam mattis eget.",
+    },
+    {
+      title: "Do you have specific princing plans to show?",
+      desc: "Donec rutrum, mauris at blandit convallis, orci nulla volutpat sapien, id pulvinar leo ligula eget nunc. In quis magna magna. Nullam mattis eget.",
+    },
+    {
+      title: "Do you have specific princing plans to show?",
+      desc: "Donec rutrum, mauris at blandit convallis, orci nulla volutpat sapien, id pulvinar leo ligula eget nunc. In quis magna magna. Nullam mattis eget.",
+    },
+    {
+      title: "Do you have specific princing plans to show?",
+      desc: "Donec rutrum, mauris at blandit convallis, orci nulla volutpat sapien, id pulvinar leo ligula eget nunc. In quis magna magna. Nullam mattis eget.",
+    },
+    {
+      title: "Do you have specific princing plans to show?",
+      desc: "Donec rutrum, mauris at blandit convallis, orci nulla volutpat sapien, id pulvinar leo ligula eget nunc. In quis magna magna. Nullam mattis eget.",
+    },
+  ];
+
+  return (
+    <Container>
+      <div className="container">
+        <div className="title dark">FAQ's</div>
+        <div className="desc dark">
+          Ko’p beriladigan savollarga javob oling.
+        </div>
+        <div className="content">
+          {data.map((e, index) => (
+            <Accardion open={open === index}>
+              <div onClick={() => setOpen(index)} className="accardion-header">
+                <div className="accardion-title">{e.title}</div>
+                <Arrow open={open === index} />
+              </div>
+              <div className="accardion-content">{e.desc}</div>
+            </Accardion>
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default FAQ;
